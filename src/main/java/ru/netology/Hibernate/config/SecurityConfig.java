3b2bb44479b866b06c79d17ef27feb66ecf3776a
Ilya Zhukov <ilyazhukovp@gmail.com>
2021-10-22 15:25:42 +0300

    spring_method_security

--- src/main/java/ru/netology/Hibernate/config/SecurityConfig.java
+++ src/main/java/ru/netology/Hibernate/config/SecurityConfig.java
@@ -3,22 +3,25 @@ package ru.netology.Hibernate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("User").password("user").roles("READ");
+        auth.inMemoryAuthentication().withUser("User1").password("user1").roles("READ","WRITE");
+        auth.inMemoryAuthentication().withUser("User2").password("user2").roles("READ", "DELETE");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().and()
-                .authorizeRequests().antMatchers("/persons").permitAll()
-                .and()
                 .authorizeRequests().anyRequest().authenticated();
     }
 }
--- src/main/java/ru/netology/Hibernate/controller/PersonsController.java
+++ src/main/java/ru/netology/Hibernate/controller/PersonsController.java
@@ -1,11 +1,14 @@
 package ru.netology.Hibernate.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.Hibernate.repository.Persons;
 import ru.netology.Hibernate.service.PersonsService;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -18,15 +21,19 @@ public class PersonsController {
     }
 
     @GetMapping("/persons/by-city")
-    public List<Persons> getPersonsByCity(@RequestParam("city") String city){
+    @Secured("ROLE_READ")
+    public List<Persons> getPersonsByCity(@RequestParam("city") String city) {
         return personsService.getPersonsByCity(city);
     }
 
     @GetMapping("/persons/less-age")
-    public List<Persons> getPersonsLessAge(@RequestParam("age") int age){
+    @RolesAllowed("ROLE_WRITE")
+    public List<Persons> getPersonsLessAge(@RequestParam("age") int age) {
         return personsService.getPersonsLessAge(age);
     }
+
     @GetMapping("/persons")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     public List<Persons> getPersonsNameSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         return personsService.getPersonsNameSurname(name, surname);
     }
